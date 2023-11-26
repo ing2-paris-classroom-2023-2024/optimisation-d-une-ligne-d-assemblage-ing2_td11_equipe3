1938bb875d5f8e10f50a737169f5aa13f12a2435
noedub <96016005+noedub@users.noreply.github.com>
2023-11-26 16:22:05 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -2,101 +2,130 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MAX_OPERATIONS 35
-#define MAX_STATIONS 35
+#define MAX_OPS 100  // Nombre maximal d'opérations
 
+// Structure pour stocker les informations d'une opération
 typedef struct {
-    int operations[MAX_OPERATIONS];
-    int compte;
-} Station;
+    int numero;
+    double temps;
+    int visite;
+    int nb_precedences;
+    int precedences[MAX_OPS];
+} Operation;
 
-typedef struct {
-    int op;
-    int contraintes;
-} OpContrainte;
-
-int comparerContraintes(const void *a, const void *b) {
-    OpContrainte *opA = (OpContrainte *)a;
-    OpContrainte *opB = (OpContrainte *)b;
-    return opB->contraintes - opA->contraintes; // Tri décroissant
-}
-
-void lirePairesExclusion(char *nomFichier, int exclusion[MAX_OPERATIONS + 1][MAX_OPERATIONS + 1], OpContrainte ops[MAX_OPERATIONS]) {
-    FILE *fichier = fopen(nomFichier, "r");
-    if (fichier == NULL) {
-        perror("Erreur d'ouverture de fichier");
-        exit(1);
+// Fonction pour lire les temps des opérations depuis un fichier
+int lire_operations(const char *nom_fichier, Operation operations[], int *nb_operations) {
+    FILE *fichier = fopen(nom_fichier, "r");
+    if (!fichier) {
+        return 0;  // Erreur lors de l'ouverture du fichier
     }
 
-    int op1, op2;
-    while (fscanf(fichier, "%d %d", &op1, &op2) == 2) {
-        exclusion[op1][op2] = 1;
-        exclusion[op2][op1] = 1;
-        ops[op1 - 1].contraintes++;
-        ops[op2 - 1].contraintes++;
-    }
-
-    for (int i = 0; i < MAX_OPERATIONS; i++) {
-        ops[i].op = i + 1;
+    int numero;
+    double temps;
+    *nb_operations = 0;
+    while (fscanf(fichier, "%d %lf\n", &numero, &temps) == 2) {
+        operations[numero - 1].numero = numero;
+        operations[numero - 1].temps = temps;
+        (*nb_operations)++;
     }
 
     fclose(fichier);
+    return 1;  // Succès
 }
 
-int peutAjouterOperation(int operation, Station station, int exclusion[MAX_OPERATIONS + 1][MAX_OPERATIONS + 1]) {
-    for (int i = 0; i < station.compte; i++) {
-        if (exclusion[operation][station.operations[i]]) {
-            return 0;
-        }
+// Fonction pour lire les préférences depuis un fichier
+int lire_precedences(const char *nom_fichier, Operation operations[], int nb_operations) {
+    FILE *fichier = fopen(nom_fichier, "r");
+    if (!fichier) {
+        return 0;  // Erreur lors de l'ouverture du fichier
     }
-    return 1;
+
+    int precedeur, successeur;
+    while (fscanf(fichier, "%d %d\n", &precedeur, &successeur) == 2) {
+        operations[precedeur - 1].precedences[operations[precedeur - 1].nb_precedences] = successeur;
+        operations[precedeur - 1].nb_precedences++;
+    }
+
+    fclose(fichier);
+    return 1;  // Succès
 }
 
-void assignerOperations(Station stations[MAX_STATIONS], int exclusion[MAX_OPERATIONS + 1][MAX_OPERATIONS + 1], OpContrainte ops[MAX_OPERATIONS]) {
-    int nombreStations = 0;
+void dfs(int v, int visited[], int *index, int stack[], Operation operations[]) {
+    visited[v] = 1;
+    for (int i = 0; i < operations[v].nb_precedences; i++) {
+        int w = operations[v].precedences[i] - 1;
+        if (!visited[w]) {
+            dfs(w, visited, index, stack, operations);
+        }
+    }
+    stack[--(*index)] = v + 1;  // Ajouter le sommet dans la pile en décrémentant l'index
+}
 
-    for (int i = 0; i < MAX_OPERATIONS; i++) {
-        int op = ops[i].op;
-        int assignee = 0;
+void tri_topologique(Operation operations[], int nb_operations, int operations_triees[]) {
+    int visited[MAX_OPS] = {0};
+    int index = nb_operations;
+    int stack[MAX_OPS];
 
-        for (int j = 0; j < nombreStations; j++) {
-            if (peutAjouterOperation(op, stations[j], exclusion)) {
-                stations[j].operations[stations[j].compte++] = op;
-                assignee = 1;
-                break;
-            }
+    for (int i = 0; i < nb_operations; i++) {
+        if (!visited[i]) {
+            dfs(i, visited, &index, stack, operations);
+        }
+    }
+
+    // Copier le résultat dans operations_triees
+    for (int i = 0; i < nb_operations; i++) {
+        operations_triees[i] = stack[i];
+    }
+}
+void equilibrer_stations(Operation operations[], int operations_triees[], int nb_operations, double temps_cycle_max) {
+    int i, num_station = 1;
+    double temps_actuel = 0.0;
+
+    printf("Station %d: ", num_station);
+    for (i = 0; i < nb_operations; i++) {
+        int op_index = operations_triees[i] - 1;  // Corriger l'index
+        double temps_operation = operations[op_index].temps;
+
+        // Vérifier si l'ajout de cette opération dépasse le temps de cycle max
+        if (temps_actuel + temps_operation > temps_cycle_max) {
+            // Passer à la station suivante et réinitialiser le temps actuel
+            num_station++;
+            temps_actuel = 0.0;
+            printf("\nStation %d: ", num_station);
         }
 
-        if (!assignee) {
-            stations[nombreStations].operations[stations[nombreStations].compte++] = op;
-            nombreStations++;
-        }
+        // Ajouter l'opération à la station actuelle
+        printf("Operation %d ", operations[op_index].numero);
+        temps_actuel += temps_operation;
     }
 }
 
-void afficherStations(Station stations[MAX_STATIONS], int nombreStations) {
-    for (int i = 0; i < nombreStations && i < 2; i++) { // Limite l'affichage aux deux premières stations
-        printf("Station %d: ", i + 1);
-        for (int j = 0; j < stations[i].compte; j++) {
-            printf("%d ", stations[i].operations[j]);
-        }
-        printf("\n");
+void afficher_ordre_topologique(int operations_triees[], int nb_operations) {
+    printf("Ordre topologique des opérations: ");
+    for (int i = 0; i < nb_operations; i++) {
+        printf("%d ", operations_triees[i]);
     }
+    printf("\n");
 }
 
 int main() {
-    Station stations[MAX_STATIONS] = {0};
-    int exclusion[MAX_OPERATIONS + 1][MAX_OPERATIONS + 1] = {0};
-    OpContrainte ops[MAX_OPERATIONS] = {0};
+    Operation operations[MAX_OPS];
+    int operations_triees[MAX_OPS];
+    int nb_operations = 0;
 
-    char *nomFichier = "exclusions.txt";
-    lirePairesExclusion(nomFichier, exclusion, ops);
+    if (!lire_operations("temps.txt", operations, &nb_operations)) {
+        fprintf(stderr, "Erreur lors de la lecture des temps d'opération.\n");
+        return 1;
+    }
 
-    qsort(ops, MAX_OPERATIONS, sizeof(OpContrainte), comparerContraintes);
+    if (!lire_precedences("precedence.txt", operations, nb_operations)) {
+        fprintf(stderr, "Erreur lors de la lecture des préférences.\n");
+        return 1;
+    }
 
-    assignerOperations(stations, exclusion, ops);
+    tri_topologique(operations, nb_operations, operations_triees);
+    equilibrer_stations(operations, operations_triees, nb_operations, 10); // Temps de cycle max de 10
 
-    afficherStations(stations, MAX_STATIONS);
-
+   // afficher_ordre_topologique(operations_triees, nb_operations);
     return 0;
 }
