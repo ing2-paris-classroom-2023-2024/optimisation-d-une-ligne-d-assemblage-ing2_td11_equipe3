18668c52562160d2bf9f70be28ff597feb3c08ff
Baptiste2301 <131976509+Baptiste2301@users.noreply.github.com>
2023-12-02 11:59:23 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -1,131 +1,426 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
-#define MAX_OPS 100  // Nombre maximal d'opérations
+#include "Graphe.h"
 
-// Structure pour stocker les informations d'une opération
-typedef struct {
-    int numero;
-    double temps;
-    int visite;
-    int nb_precedences;
-    int precedences[MAX_OPS];
-} Operation;
 
-// Fonction pour lire les temps des opérations depuis un fichier
-int lire_operations(const char *nom_fichier, Operation operations[], int *nb_operations) {
-    FILE *fichier = fopen(nom_fichier, "r");
-    if (!fichier) {
-        return 0;  // Erreur lors de l'ouverture du fichier
-    }
+// Fonction recursive pour trouver et afficher tous les chemins de l'entree a la sortie
+void trouver_tous_les_chemins(Graphe* graphe, int sommet_courant, int sommet_sortie, int* visite, int* chemin, int index) {
+    visite[sommet_courant] = 1;
+    chemin[index] = sommet_courant;
+    index++;
 
-    int numero;
-    double temps;
-    *nb_operations = 0;
-    while (fscanf(fichier, "%d %lf\n", &numero, &temps) == 2) {
-        operations[numero - 1].numero = numero;
-        operations[numero - 1].temps = temps;
-        (*nb_operations)++;
-    }
-
-    fclose(fichier);
-    return 1;  // Succès
-}
-
-// Fonction pour lire les préférences depuis un fichier
-int lire_precedences(const char *nom_fichier, Operation operations[], int nb_operations) {
-    FILE *fichier = fopen(nom_fichier, "r");
-    if (!fichier) {
-        return 0;  // Erreur lors de l'ouverture du fichier
-    }
-
-    int precedeur, successeur;
-    while (fscanf(fichier, "%d %d\n", &precedeur, &successeur) == 2) {
-        operations[precedeur - 1].precedences[operations[precedeur - 1].nb_precedences] = successeur;
-        operations[precedeur - 1].nb_precedences++;
-    }
-
-    fclose(fichier);
-    return 1;  // Succès
-}
-
-void dfs(int v, int visited[], int *index, int stack[], Operation operations[]) {
-    visited[v] = 1;
-    for (int i = 0; i < operations[v].nb_precedences; i++) {
-        int w = operations[v].precedences[i] - 1;
-        if (!visited[w]) {
-            dfs(w, visited, index, stack, operations);
+    if (sommet_courant == sommet_sortie) {
+        // Nous avons trouve un chemin de l'entree a la sortie, l'afficher
+        printf("Chemin trouve : ");
+        for (int i = 0; i < index; i++) {
+            printf("%d", chemin[i]);
+            if (i < index - 1) {
+                printf(" -> ");
+            }
         }
-    }
-    stack[--(*index)] = v + 1;  // Ajouter le sommet dans la pile en décrémentant l'index
-}
-
-void tri_topologique(Operation operations[], int nb_operations, int operations_triees[]) {
-    int visited[MAX_OPS] = {0};
-    int index = nb_operations;
-    int stack[MAX_OPS];
-
-    for (int i = 0; i < nb_operations; i++) {
-        if (!visited[i]) {
-            dfs(i, visited, &index, stack, operations);
+        printf("\n");
+    } else {
+        pArc arc = graphe->pSommet[sommet_courant]->arc;
+        while (arc != NULL) {
+            int voisin = arc->sommet;
+            if (!visite[voisin]) {
+                trouver_tous_les_chemins(graphe, voisin, sommet_sortie, visite, chemin, index);
+            }
+            arc = arc->arc_suivant;
         }
     }
 
-    // Copier le résultat dans operations_triees
-    for (int i = 0; i < nb_operations; i++) {
-        operations_triees[i] = stack[i];
-    }
+    // Marquer le sommet courant comme non visite pour permettre d'explorer d'autres chemins
+    visite[sommet_courant] = 0;
 }
-void equilibrer_stations(Operation operations[], int operations_triees[], int nb_operations, double temps_cycle_max) {
-    int i, num_station = 1;
-    double temps_actuel = 0.0;
 
-    printf("Station %d: ", num_station);
-    for (i = 0; i < nb_operations; i++) {
-        int op_index = operations_triees[i] - 1;  // Corriger l'index
-        double temps_operation = operations[op_index].temps;
+// Fonction principale pour trouver et afficher tous les chemins de l'entree a la sortie
+void trouver_tous_les_chemins_principale(Graphe* graphe, int sommet_initial, int sommet_sortie) {
+    int* visite = (int*)malloc(graphe->ordre * sizeof(int));
+    int* chemin = (int*)malloc(graphe->ordre * sizeof(int));
 
-        // Vérifier si l'ajout de cette opération dépasse le temps de cycle max
-        if (temps_actuel + temps_operation > temps_cycle_max) {
-            // Passer à la station suivante et réinitialiser le temps actuel
-            num_station++;
-            temps_actuel = 0.0;
-            printf("\nStation %d: ", num_station);
+    for (int i = 0; i < graphe->ordre; i++) {
+        visite[i] = 0;
+    }
+
+    printf("Tous les chemins possibles de l'entree (%d) a la sortie (%d):\n", sommet_initial, sommet_sortie);
+    trouver_tous_les_chemins(graphe, sommet_initial, sommet_sortie, visite, chemin, 0);
+
+    free(visite);
+    free(chemin);
+}
+
+void DetruireFile(File file) {
+    free(file->tableau);
+    free(file);
+}
+
+
+int FileVide(File file) {
+    return (file->taille == 0);
+}
+
+
+int Defiler(File file) {
+    if (FileVide(file)) {
+        printf("La file est vide. Impossible de defiler.\n");
+        return -1; // Valeur d'erreur, a adapter en fonction de vos besoins.
+    }
+
+    int valeur = file->tableau[file->front];
+    file->front = (file->front + 1) % file->capacite;
+    file->taille--;
+    return valeur;
+}
+
+
+int FilePleine(File file) {
+    return (file->taille == file->capacite);
+}
+
+void Enfiler(File file, int item) {
+    if (FilePleine(file)) {
+        printf("La file est pleine. Impossible d'enfiler.\n");
+        return;
+    }
+
+    file->rear = (file->rear + 1) % file->capacite;
+    file->tableau[file->rear] = item;
+    file->taille++;
+}
+
+
+File CreerFile() {
+    File file = (File)malloc(sizeof(struct File));
+    if (!file) {
+        // Gestion de l'echec de l'allocation de memoire
+        exit(1);
+    }
+    file->capacite = 100; // Remplacez par la capacite souhaitee
+    file->front = file->taille = 0;
+    file->rear = -1;
+    file->tableau = (int*)malloc(file->capacite * sizeof(int));
+    if (!file->tableau) {
+        // Gestion de l'echec de l'allocation de memoire
+        exit(1);
+    }
+    return file;
+}
+
+/* affichage des successeurs du sommet num*/
+void afficher_successeurs(pSommet * sommet, int num)
+{
+
+    printf(" sommet %d :\n",num+1);
+
+    pArc arc=sommet[num]->arc;
+
+    while(arc!=NULL)
+    {
+        printf("%d ",arc->sommet);
+        arc=arc->arc_suivant;
+    }
+
+}
+
+// Ajouter l'arête entre les sommets s1 et s2 du graphe
+pSommet* CreerArete(pSommet* sommet,int s1,int s2)
+{
+    if(sommet[s1]->arc==NULL)
+    {
+        pArc Newarc=(pArc)malloc(sizeof(struct Arc));
+        Newarc->sommet=s2;
+        Newarc->arc_suivant=NULL;
+        sommet[s1]->arc=Newarc;
+        return sommet;
+    }
+
+    else
+    {
+        pArc temp=sommet[s1]->arc;
+        while( !(temp->arc_suivant==NULL))
+        {
+            temp=temp->arc_suivant;
+        }
+        pArc Newarc=(pArc)malloc(sizeof(struct Arc));
+        Newarc->sommet=s2;
+        Newarc->arc_suivant=NULL;
+
+        if(temp->sommet>s2)
+        {
+            Newarc->arc_suivant=temp->arc_suivant;
+            Newarc->sommet=temp->sommet;
+            temp->sommet=s2;
+            temp->arc_suivant=Newarc;
+            return sommet;
         }
 
-        // Ajouter l'opération à la station actuelle
-        printf("Operation %d ", operations[op_index].numero);
-        temps_actuel += temps_operation;
+        temp->arc_suivant=Newarc;
+        return sommet;
     }
 }
 
-void afficher_ordre_topologique(int operations_triees[], int nb_operations) {
-    printf("Ordre topologique des opérations: ");
-    for (int i = 0; i < nb_operations; i++) {
-        printf("%d ", operations_triees[i]);
+// creer le graphe
+Graphe* CreerGraphe(int ordre)
+{
+    Graphe * Newgraphe=(Graphe*)malloc(sizeof(Graphe));
+    Newgraphe->pSommet = (pSommet*)malloc(ordre*sizeof(pSommet));
+
+    for(int i=0; i<ordre; i++)
+    {
+        Newgraphe->pSommet[i]=(pSommet)malloc(sizeof(struct Sommet));
+        Newgraphe->pSommet[i]->valeur=i;
+        Newgraphe->pSommet[i]->arc=NULL;
     }
+    return Newgraphe;
+}
+
+
+/* La construction du reseau peut se faire a partir d'un fichier dont le nom est passe en paramètre
+Le fichier contient : ordre, taille,orientation (0 ou 1)et liste des arcs */
+Graphe * lire_graphe(char * nomFichier)
+{
+    Graphe* graphe;
+    FILE * ifs = fopen(nomFichier,"r");
+    int taille, orientation, ordre, s1, s2;
+
+    if (!ifs)
+    {
+        printf("Erreur de lecture fichier\n");
+        exit(-1);
+    }
+
+    fscanf(ifs,"%d",&ordre);
+
+    graphe=CreerGraphe(ordre); // creer le graphe d'ordre sommets
+
+    fscanf(ifs,"%d",&taille);
+    fscanf(ifs,"%d",&orientation);
+
+    graphe->orientation=orientation;
+    graphe->ordre=ordre;
+
+    // creer les arêtes du graphe
+    for (int i=0; i<taille; ++i)
+    {
+        fscanf(ifs,"%d%d",&s1,&s2);
+        graphe->pSommet=CreerArete(graphe->pSommet, s1, s2);
+
+        if(!orientation)
+            graphe->pSommet=CreerArete(graphe->pSommet, s2, s1);
+    }
+
+    return graphe;
+}
+
+/*affichage du graphe avec les successeurs de chaque sommet */
+void graphe_afficher(Graphe* graphe)
+{
+    printf("graphe\n");
+
+    if(graphe->orientation)
+        printf("oriente\n");
+    else
+        printf("non oriente\n");
+
+    printf("ordre = %d\n",graphe->ordre);
+
+    printf("listes d'adjacence :\n");
+
+    for (int i=0; i<graphe->ordre; i++)
+
+    {
+        afficher_successeurs(graphe->pSommet, i);
+        printf("\n");
+    }
+
+}
+
+
+/* Fonction pour le parcours en largeur BFS*/
+void BFS(Graphe* graphe, int sommet_depart) {
+    // Assurez-vous que sommet_depart est valide
+    if (sommet_depart < 0 || sommet_depart >= graphe->ordre) {
+        printf("Sommet de depart invalide\n");
+        return;
+    }
+
+    // Tableau pour marquer les sommets visites
+    int* visite = (int*)malloc(graphe->ordre * sizeof(int));
+    for (int i = 0; i < graphe->ordre; i++) {
+        visite[i] = 0;
+    }
+
+    // File pour effectuer le BFS
+    File* file = CreerFile();
+    visite[sommet_depart] = 1;
+    Enfiler(file, sommet_depart);
+
+    printf("Parcours en largeur (BFS) a partir du sommet %d :\n", sommet_depart);
+
+    while (!FileVide(file)) {
+        int sommet_courant = Defiler(file);
+        printf("%d ", sommet_courant);
+
+        // Parcourez les voisins du sommet courant
+        pArc arc = graphe->pSommet[sommet_courant]->arc;
+        while (arc != NULL) {
+            int voisin = arc->sommet;
+            if (!visite[voisin]) {
+                visite[voisin] = 1;
+                Enfiler(file, voisin);
+            }
+            arc = arc->arc_suivant;
+        }
+    }
+
     printf("\n");
+
+    free(visite);
+    DetruireFile(file);
+}
+
+/* Fonction pour le parcours en profondeur DFS rescursif */
+void DFSRecursif(Graphe* graphe, int sommet, int* visite) {
+    visite[sommet] = 1;
+    printf("%d ", sommet);
+
+    // Parcourez les voisins du sommet
+    pArc arc = graphe->pSommet[sommet]->arc;
+    while (arc != NULL) {
+        int voisin = arc->sommet;
+        if (!visite[voisin]) {
+            DFSRecursif(graphe, voisin, visite);
+        }
+        arc = arc->arc_suivant;
+    }
+}
+
+/* Fonction pour le parcours en profondeur DFS */
+void DFS(Graphe* graphe, int sommet_depart) {
+    // Assurez-vous que sommet_depart est valide
+    if (sommet_depart < 0 || sommet_depart >= graphe->ordre) {
+        printf("Sommet de depart invalide\n");
+        return;
+    }
+
+    int* visite = (int*)malloc(graphe->ordre * sizeof(int));
+    for (int i = 0; i < graphe->ordre; i++) {
+        visite[i] = 0;
+    }
+
+    printf("Parcours en profondeur (DFS) a partir du sommet %d :\n", sommet_depart);
+    DFSRecursif(graphe, sommet_depart, visite);
+
+    printf("\n");
+
+    free(visite);
+}
+
+
+
+
+
+// Fonction récursive pour explorer les sommets d'une composante connexe
+void explorer_composante_connexe(Graphe* graphe, int sommet, int* visite, int* sommets_composante, int* taille_composante) {
+    visite[sommet] = 1;
+    sommets_composante[(*taille_composante)++] = sommet;
+
+    pArc arc = graphe->pSommet[sommet]->arc;
+    while (arc != NULL) {
+        int voisin = arc->sommet;
+        if (!visite[voisin]) {
+            explorer_composante_connexe(graphe, voisin, visite, sommets_composante, taille_composante);
+        }
+        arc = arc->arc_suivant;
+    }
+}
+
+// Fonction pour rechercher et afficher les composantes connexes d'un graphe non orienté
+void rechercher_et_afficher_composantes_connexes(Graphe* graphe) {
+    int* visite = (int*)malloc(graphe->ordre * sizeof(int));
+
+    for (int i = 0; i < graphe->ordre; i++) {
+        visite[i] = 0;
+    }
+
+    int composante_connexe = 0;
+
+    printf("Composantes connexes du graphe  :\n");
+
+    for (int sommet = 0; sommet < graphe->ordre; sommet++) {
+        if (!visite[sommet] && sommet != 0 && sommet != 1) {
+            composante_connexe++;
+            int* sommets_composante = (int*)malloc(graphe->ordre * sizeof(int));
+            int taille_composante = 0;
+
+            printf("Composante connexe %d : ", composante_connexe);
+
+            // Appel à la fonction explorer_composante_connexe pour explorer la composante
+            explorer_composante_connexe(graphe, sommet, visite, sommets_composante, &taille_composante);
+
+            // Trier les sommets de la composante connexe
+            qsort(sommets_composante, taille_composante, sizeof(int), comparaison_entiers);
+
+            // Afficher les sommets triés
+            for (int i = 0; i < taille_composante; i++) {
+                printf("%d ", sommets_composante[i]);
+            }
+
+            printf("\n");
+
+            free(sommets_composante);
+        }
+    }
+
+    free(visite);
+}
+
+// Fonction pour trier
+int comparaison_entiers(const void* a, const void* b) {
+    return (*(int*)a - *(int*)b);
 }
 
 int main() {
-    Operation operations[MAX_OPS];
-    int operations_triees[MAX_OPS];
-    int nb_operations = 0;
+    Graphe * g;
+    char nom_fichier[50];
 
-    if (!lire_operations("temps.txt", operations, &nb_operations)) {
-        fprintf(stderr, "Erreur lors de la lecture des temps d'opération.\n");
-        return 1;
-    }
+    printf("Entrer le nom du fichier du labyrinthe: ");
+    fgets(nom_fichier, sizeof(nom_fichier), stdin);
+    // Supprime le saut de ligne (\n) de la fin de la chaîne de caractères.
+    nom_fichier[strcspn(nom_fichier, "\n")] = '\0';
 
-    if (!lire_precedences("precedence.txt", operations, nb_operations)) {
-        fprintf(stderr, "Erreur lors de la lecture des préférences.\n");
-        return 1;
-    }
+    g = lire_graphe(nom_fichier);
 
-    tri_topologique(operations, nb_operations, operations_triees);
-    equilibrer_stations(operations, operations_triees, nb_operations, 10); // Temps de cycle max de 10
+    int sommet_initial;
+    printf("Numero du sommet initial : ");
+    scanf("%d", &sommet_initial);
 
-   // afficher_ordre_topologique(operations_triees, nb_operations);
+    // Afficher le graphe
+    graphe_afficher(g);
+
+    // Effectuer le BFS et DFS avec le sommet initial
+    BFS(g, sommet_initial);
+    DFS(g, sommet_initial);
+
+
+
+
+    int sommet_sortie;
+
+
+    printf("Numero du sommet de sortie : ");
+    scanf("%d", &sommet_sortie);
+
+    // Afficher le graphe
+    graphe_afficher(g);
+
+    // Appeler la fonction pour trouver et afficher tous les chemins possibles
+    trouver_tous_les_chemins_principale(g, sommet_initial, sommet_sortie);
+
+    // Rechercher et afficher les composantes connexes
+    rechercher_et_afficher_composantes_connexes(g);
+
     return 0;
+
+
 }
